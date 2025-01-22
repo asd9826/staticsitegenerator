d199d5e73081b3cad935995b9343f737f20dd29d
asd9826 <anildeo2601@gmail.com>
2025-01-22 13:58:31 +1100

    Added extract images and links

--- src/inline.py
+++ src/inline.py
@@ -1,4 +1,5 @@
 from textnode import TextNode,TextType
+import re
 
 def split_nodes_delimiter(old_nodes,delimiter, text_type):
     new_nodes = []
@@ -28,3 +29,13 @@ def split_nodes_delimiter(old_nodes,delimiter, text_type):
             else:
                 new_nodes.append(TextNode(split_node[i],text_type))
     return new_nodes
+
+def extract_markdown_images(text):
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern,text)
+    return matches
+
+def extract_markdown_links(text):
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern,text)
+    return matches
