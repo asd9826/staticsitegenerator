3ef886f9cf567335072dc6d65ec7d16b4577bc1c
asd9826 <anildeo2601@gmail.com>
2025-01-23 21:18:45 +1100

    Add text_to_textnode function

--- src/inline.py
+++ src/inline.py
@@ -103,3 +103,12 @@ def extract_markdown_links(text):
     pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
     matches = re.findall(pattern,text)
     return matches
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes,"**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes,"*", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_images(nodes)
+    nodes = split_nodes_links(nodes)
+    return nodes
